e7b12d0fd27b718e9cf5a55b44cefb73ad376c16
Tereza Bright <tereza.stankova1@gmail.com>
2024-09-24 14:04:30 +0200

    add Profile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import './App.css'
+import './App.css';
+import user from '../data/user.json';
+import { Profile } from './components/profile/Profile';
+
 
 const App = () => {
 return (
-  <div>Zde budou přidány vaše nové komponenty</div>
+    <div><Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+    /></div>
 )}
 
 export default App
